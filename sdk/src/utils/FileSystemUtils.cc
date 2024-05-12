90d8ebe6a2dcd8aaeb68e7cf3156732faf1f8aee
ersonw <63575687+ersonw@users.noreply.github.com>
2024-05-12 11:33:39 +0800

    first commit

--- sdk/src/utils/FileSystemUtils.cc
+++ sdk/src/utils/FileSystemUtils.cc
@@ -47,21 +47,8 @@
 #define  oss_stat       stat
 #endif
 using namespace AlibabaCloud::OSS;
-std::string ws2s(const std::wstring &ws)
-{
-    size_t i;
-    std::string curLocale = setlocale(LC_ALL, NULL);
-    setlocale(LC_ALL, "chs");
-    const wchar_t* _source = ws.c_str();
-    size_t _dsize = 2 * ws.size() + 1;
-    char* _dest = new char[_dsize];
-    memset(_dest, 0x0, _dsize);
-    wcstombs_s(&i, _dest, _dsize, _source, _dsize);
-    std::string result = _dest;
-    delete[] _dest;
-    setlocale(LC_ALL, curLocale.c_str());
-    return result;
-}
+
+
 
 bool AlibabaCloud::OSS::CreateDirectory(const std::string &folder)
 {
@@ -142,7 +129,21 @@ bool AlibabaCloud::OSS::IsFileExist(const std::string& file)
 
 //wchar path
 #ifdef _WIN32
-
+std::string ws2s(const std::wstring &ws)
+{
+    size_t i;
+    std::string curLocale = setlocale(LC_ALL, NULL);
+    setlocale(LC_ALL, "chs");
+    const wchar_t* _source = ws.c_str();
+    size_t _dsize = 2 * ws.size() + 1;
+    char* _dest = new char[_dsize];
+    memset(_dest, 0x0, _dsize);
+    wcstombs_s(&i, _dest, _dsize, _source, _dsize);
+    std::string result = _dest;
+    delete[] _dest;
+    setlocale(LC_ALL, curLocale.c_str());
+    return result;
+}
 bool AlibabaCloud::OSS::CreateDirectory(const std::wstring &folder)
 {
     std::wstring folder_builder;
